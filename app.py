898c850145314cf255fb6d9ae93c2db4add91e0b
sidhusmart <sidharth.ramachandran@gmail.com>
2020-07-14 23:38:57 +0200

    Adding a new API deployment

--- app.py
+++ app.py
@@ -43,12 +43,52 @@ def predict():
     response["prediction"] = sentiment_label[output.item()]
   return jsonify(response)
 
+@app.route('/api/v1/classifybugs', methods=['POST'])
+def bugs_classification():
+  """API Endpoint used for serving the classification function of the Bugs Analysis model
+
+  The classifier was trained using the JDT bugs dataset from BugZilla 
+  When the text of a bug report is passed, it is cleaned and one of the five identified categories are returned APT, Core, Debug, Doc, Text, UI
+
+  ---
+  parameters:
+    - in: body 
+      name: body 
+      schema:
+        id: schema_id
+        required:
+          - bug_report
+        properties:
+          bug_report:
+            type: string
+            description: Text from the bug report that is to be classified
+            default: "I tried to build and deploy this script but got a ResourceNotFoundException"
+  responses:
+    200:
+      description: SUCCESS
+      examples: {"prediction":"CORE"}
+  """
+  bugs_label = ["APT", "Core", "Debug", "Doc", "Text", "UI"]
+  response = dict()
+  if request.method == 'POST':
+    data = request.get_json()
+    text_clean = preprocessing.clean(data['review_text'])
+    text_tfidf = tfidf_vectorizer_bugs.transform([text_clean])
+    output = bugs_classifier.predict(text_tfidf)
+    response["prediction"] = output.item()
+  return jsonify(response)
+
+
 def run_app():
   try:
     global saved_model, tfidf_vectorizer
     saved_model = pickle.load(open('models/sentiment_classification.pickle', 'rb'))
     tfidf_vectorizer = pickle.load(open('models/tfidf_vectorizer.pickle', 'rb'))
-    print('model loaded')
+    print('sentiment models loaded')
+    global bugs_classifier, tfidf_vectorizer_bugs
+    bugs_classifier = pickle.load(open('models/bugs_classifier.pickle', 'rb'))
+    tfidf_vectorizer_bugs = pickle.load(open('models/tfidf_vectorizer_bugs.pickle', 'rb'))
+    print('bugs classification models loaded')
   except Exception as e:
     raise ValueError('No model here')
   return app
